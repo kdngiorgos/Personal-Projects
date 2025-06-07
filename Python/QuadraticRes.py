af2a11b0b0dff6a084b4434a82ab0a37d6ffb608
kdngiorgos <kdngiorgos@gmail.com>
2025-06-07 12:23:58 +0300

    Added more functionality

--- Python/QuadraticRes.py
+++ Python/QuadraticRes.py
@@ -1,17 +1,52 @@
 import math
 from collections import defaultdict
 
+def BruteForce(p):
+    hashmap = defaultdict(list)
+    for i in range(1, p):
+        remainder = i**2 % p
+        hashmap[remainder].append(i) 
+    return hashmap
 
-print("Modulo: ")
-p = int(input())
 
-qres = defaultdict(list)
+def LegendreSymbol(a, p):
+    if a == 0:
+        return 0
+    return pow(a, (p - 1) // 2, p)
 
-for i in range(1, p):
-    remainder = i**2 % p
-    qres[remainder].append(i) 
+def LegendreResidues(p):
+    hashmap = defaultdict(list)
+    for i in range(1, p):
+        if LegendreSymbol(i, p) == 1:
+            remainder = i**2 % p
+            hashmap[remainder].append(i)
+    return hashmap
 
-sorted_hashmap = {k: qres[k] for k in sorted(qres)}    
 
-print(f"Quadratic residues modulo {p}:")
-print(f"{sorted_hashmap}")
+def main():
+    p = int(input("Enter a modulo p: "))
+    ch = input("Do you want to use brute force (b) or Legendre (l) or Specific Integers (s)? ")
+    
+    sorted_hashmap = {}
+    match ch:
+        case 'b':
+            qres = BruteForce(p)
+            sorted_hashmap = {k: qres[k] for k in sorted(qres)}  
+        case 'l':
+            qres = LegendreResidues(p)
+            sorted_hashmap = {k: qres[k] for k in sorted(qres)}
+        case 's':
+            while True:
+                temp  = int(input("Give me a number):"))
+                if temp < 0 or temp >= p:
+                    print(f"Number must be in the range [0, {p-1}].")
+                    break
+                if LegendreSymbol(temp, p) == 1:
+                    print(f"{temp} is a quadratic residue modulo {p}.")
+    if sorted_hashmap:             
+        print(f"Quadratic residues modulo {p}:")
+        print(f"{sorted_hashmap}")
+
+
+if __name__ == "__main__":
+    main()
